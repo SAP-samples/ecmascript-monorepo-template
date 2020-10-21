855afe7ac1819d173b29f8942f91e5eb401d1e38
Shahar Soel <shachar.soel@sap.com>
2020-10-21 19:49:04 +0300

    fix: intermediately breakpoints may not be hit

---
+++ packages/vscode_custom_editor_vue_frontend/test/fix-debugging.spec.js
@@ -0,0 +1,5 @@
+// During tests sources are compiled on the fly
+// This debugger statement provides our IDE with a little grace period
+// so it will be able to attach the breakpoints in time.
+// eslint-disable-next-line no-debugger
+debugger;
