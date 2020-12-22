c59d80aa4034468c4133c1825db0890ca448b9ea
Shahar Soel <shachar.soel@sap.com>
2020-12-22 20:45:54 +0200

    feat: improve eslint config

--- packages/vscode_simple_ext/src/extension.ts
+++ packages/vscode_simple_ext/src/extension.ts
@@ -3,7 +3,7 @@ import { window } from "vscode";
 import { multiply } from "@ecmascript_monorepo_template/npm_package_javascript_library";
 import { add } from "@ecmascript_monorepo_template/npm_package_typescript_library";
 
-export async function activate(): Promise<void> {
+export function activate(): void {
   const channel = window.createOutputChannel("vscode-simple_ext");
   channel.appendLine("Hello Cruel World");
   channel.appendLine(`two multiplied by three equals: ${multiply(2, 3)}`);
